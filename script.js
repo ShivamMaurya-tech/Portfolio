93ac868acdd5d3d043ef4e8ce7fdfee3150ea476
Shivam Maurya <shivammaurya224466@gmail.com>
2026-07-04 21:53:38 +0530

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+// =========================
+// Contact Form
+// =========================
+
+const form =
+    document.querySelector(".contact-form");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    alert("✅ Thank you! Your message has been sent.");
+
+    form.reset();
+
+});
+
